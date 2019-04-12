2e966634253d10dd62a07c3397bf40e79abe8dfc
Maurice de Laat <info@mauricedelaat.nl>
2019-04-12 10:24:55 +0200

    Moved over NameUtility and base ModelTransform from @yellicode/templating.

---
+++ src/model-transform.ts
@@ -0,0 +1,68 @@
+﻿/*
+ * Copyright (c) 2019 Yellicode
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+
+/**
+ * The base interface for all model transformations.
+ */
+export interface ModelTransform<TSource, TTarget = TSource> {
+    /**
+     * Transforms the source model into the target model.
+     */
+    transform(source: TSource): TTarget;
+}
+
+/**
+ * Performs a sequence of model transforms, where the output of each transform is
+ * the input of the next transform.
+ */
+export class ModelTransformPipeline<TModel = any> implements ModelTransform<TModel> {
+    private transforms: ModelTransform<TModel>[];
+
+    /**
+     * Creates a new ModelTransformPipeline, optionally initialized with a collection of transforms.
+     * @param transform sAny transforms to initialize the pipeline with.
+     */
+    constructor(...transforms: ModelTransform<TModel>[]) {
+        this.transforms = transforms || [];
+    }
+
+    /**
+    * Adds a new collection of transforms to the pipeline.
+    * @param transforms The transforms to be added.
+    */
+    public addRange(...transforms: ModelTransform<TModel>[]): void {
+        transforms.forEach(t => {
+            this.transforms.push(t);
+        });
+    }
+
+    /**
+     * Adds a new transform to the pipeline.
+     * @param transform The transform to be added.
+     */
+    public add(transform: ModelTransform<TModel>): void {
+        if (!transform) return;
+
+        this.transforms.push(transform);
+    }
+
+    /**
+    * Transforms the source model by applying the specified transforms in sequence.
+    * @returns The output of the last transform that was applied.
+    */
+    public transform(model: TModel): TModel {
+        if (this.transforms == null)
+            return model;
+
+        let transformedModel = model;
+        this.transforms.forEach(t => {
+            transformedModel = t.transform(transformedModel);
+        });
+        return transformedModel;
+    }
+}
---
+++ src/name-utility.ts
@@ -0,0 +1,139 @@
+/*
+ * Copyright (c) 2019 Yellicode
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ */
+import { StringUtility } from './string-utility';
+
+/**
+ * Provides helper functions for converting names.
+ */
+export class NameUtility {
+    /**
+    * Makes the first character of the string uppercase.     
+    */
+    public static capitalize(input: string): string {
+        if (!input) return input;
+        return StringUtility.capitalize(input);
+    }
+
+    /**
+     * Makes the first character of the string lowercase.     
+     */
+    public static unCapitalize(input: string): string {
+        if (!input) return input;
+        return StringUtility.unCapitalize(input);
+    }
+
+    /**
+     * Converts a UpperCamelCase or lowerCamelCase string to a kebab-case (aka dash-case) string.
+     * @param input A camelCase or UpperCamelCase string.
+     * @returns a kebab-case (aka dash-case) string, meaning all lowercase with a dash separating words.
+     */
+    public static camelToKebabCase(input: string): string {
+        if (!input) return input;
+
+        const result: string[] = [];
+
+        for (var i = 0, len = input.length; i < len; i++) {
+            var c = input.charAt(i);
+            const upperCase = c.toUpperCase();
+            const lowerCase = c.toLowerCase();
+            const isUpperCase = (c === upperCase && c !== lowerCase);
+            if (isUpperCase && i > 0 && i < len - 1) {
+                let insertHyphen = true;
+                if (i < len - 1 && StringUtility.isUpperCase(input.charAt(i + 1))) {
+                    // The next character is also uppercase
+                    insertHyphen = false;
+                }
+                if (insertHyphen) {
+                    result.push('-');
+                }
+            }
+            result.push(lowerCase);
+        }
+        return result.join('');
+    }
+
+
+    /**
+     * Converts a UpperCamelCase string to a lowerCamelCase string.
+     * @param input A UpperCamelCase string.
+     * @returns a lowerCamelCase string.
+     */
+    public static upperToLowerCamelCase(input: string): string {
+        if (!input) return input;
+
+        let done = false;
+        let isPreviousMatch = false;
+
+        const result: string[] = new Array(input.length);
+        for (var i = 0, len = input.length; i < len; i++) {
+            var c = input.charAt(i);
+            if (!done) {
+                const upperCase = c.toUpperCase();
+                const lowerCase = c.toLowerCase();
+                const isMatch = (c === upperCase && c !== lowerCase);
+                if (isMatch) {
+                    // The character is upperCase, make it lowerCase      
+                    c = lowerCase;
+                }
+                else {
+                    // The character is lowerCase. We are done, but..                
+                    if (isPreviousMatch && i > 1) {
+                        // ... if we converted the previous char to lowercase, make it uppercase again.    
+                        // For example: ISBNNumber would become isbnNumber instead of isbnnumber
+                        result[i - 1] = result[i - 1].toUpperCase();
+                    }
+                    done = true;
+                }
+                isPreviousMatch = isMatch;
+            }
+            result[i] = c;
+        }
+        return result.join('');
+    }
+
+    /**
+   * Converts a lowerCamelCase string to a UpperCamelCase string.
+   * @param input A lowerCamelCase string.
+   * @returns a UpperCamelCase string.
+   */
+    public static lowerToUpperCamelCase(input: string): string {
+        return StringUtility.capitalize(input);
+    }
+
+    /**
+     * Converts a kebab-case, lowerCamelCase or UpperCamelCase string to an ALL_UPPER_CASE string.
+     * @param input A kebab-case (aka dash-case), lowerCamelCase or UpperCamelCase string.
+     * @returns an ALL_UPPER_CASE string.
+     */
+    public static toAllUpperCase(input: string): string {
+        if (!input) return input;
+
+        const result: string[] = [];
+
+        for (var i = 0, len = input.length; i < len; i++) {
+            var c = input.charAt(i);
+            const upperCase = c.toUpperCase();
+            const lowerCase = c.toLowerCase();
+            const isUpperCase = (c === upperCase && c !== lowerCase);
+            const isDash = c === '-';
+            if ((isDash || isUpperCase) && i > 0 && i < len - 1) {
+                let insertHyphen = true;
+                if (i < len - 1 && StringUtility.isUpperCase(input.charAt(i + 1))) {
+                    // The next character is also uppercase
+                    insertHyphen = false;
+                }
+                if (insertHyphen) {
+                    result.push('_');
+                }
+            }
+            if (!isDash)
+                result.push(upperCase);
+        }
+        return result.join('');
+    }
+}
